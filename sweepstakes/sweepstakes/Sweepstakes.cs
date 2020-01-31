f2ccc22467595f47f259b11924a939cc6ab8f8e3
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-31 13:15:25 -0600

    Added methods to Sweepstakes

--- sweepstakes/sweepstakes/Sweepstakes.cs
+++ sweepstakes/sweepstakes/Sweepstakes.cs
@@ -10,11 +10,24 @@ namespace sweepstakes
     {
         Dictionary<int, Contestant> contestants;
         string name;
-        public string Name;
+        public string Name { get; }
 
         public void RegisterContestant(Contestant contestant)
         {
+            contestants.Add(contestant.RegistrationNumber, contestant);
+        }
+        public Contestant PickWinner()
+        {
+            // pick a random winner, return contestant object
+            Random random = new Random();
+            int key = random.Next(contestants.Count);
+            Contestant contestant = contestants[key];
+            return contestant;
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
 
         }
+        
     }
 }
