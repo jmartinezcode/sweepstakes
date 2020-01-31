1a48b449043ea21437767cae8fd923e1dfcdfc22
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-31 11:30:32 -0600

    Added UserInterface

---
+++ sweepstakes/sweepstakes/UserInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    public static class UserInterface
+    {
+        public static string GetUserInputFor(string prompt)
+        {
+            return prompt;
+        }
+    }
+}
