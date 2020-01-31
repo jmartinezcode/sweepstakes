ca04985cebe6a6f8d00c70e576a02e8c54930e45
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-31 11:47:31 -0600

    Added ISweepstakesManager interface

--- sweepstakes/sweepstakes/UserInterface.cs
+++ sweepstakes/sweepstakes/UserInterface.cs
@@ -8,6 +8,16 @@ namespace sweepstakes
 {
     public static class UserInterface
     {
+        /*
+         * create a user interface for any information the application would need to get from the user. 
+         * One example would be the functionality to assign a Contestant object:
+         *      first name, 
+         *      last name, 
+         *      email address, 
+         *      registration number.
+         * 
+         */
+
         public static string GetUserInputFor(string prompt)
         {
             return prompt;
