b994b88f306e259de58a91c3ac5ad647598f81cd
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-31 13:37:53 -0600

    Added inheritance to Stack and Queue classes

--- sweepstakes/sweepstakes/SweepstakesQueueManager.cs
+++ sweepstakes/sweepstakes/SweepstakesQueueManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace sweepstakes
 {
-    class SweepstakesQueueManager
+    class SweepstakesQueueManager: ISweepstakesManager  
     {
         Queue<Sweepstakes> queue;
 
--- sweepstakes/sweepstakes/SweepstakesStackManager.cs
+++ sweepstakes/sweepstakes/SweepstakesStackManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace sweepstakes
 {
-    class SweepstakesStackManager
+    class SweepstakesStackManager : ISweepstakesManager
     {
         Stack<Sweepstakes> stack;
 
